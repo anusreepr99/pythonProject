3c3bd91786468418c6604dce29ac951e7dec5ba3
anusreepr99 <anusrepr99@gmail.com>
2021-03-24 14:09:48 +0530

    msg5

---
+++ CO3/import area perimeter.py
@@ -0,0 +1,12 @@
+from graphics import circle as c
+from graphics import rectangle as r
+from graphics.dgraphics import cuboid as cu
+from graphics.dgraphics import sphere as s
+print(c.area(2))
+print(c.peri(2))
+print(r.area(2,2))
+print(r.peri(2,2))
+print(cu.area(2,2,2))
+print(cu.peri(2,2,2))
+print(s.area(2))
+print(s.peri(2))
